d8c438988868414b219bb619810a50f2220ad7db
Amós Batista <amos.silva@gmail.com>
2022-03-07 20:59:04 -0300

    Create TesteAmos.vue

---
+++ pages/TesteAmos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="page">
+    <page-header />
+    <nuxt-content :document="page" />
+    <page-footer />
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const page = await $content('pages/teste-amos').fetch()
+    return {
+      page
+    }
+  }
+}
+</script>
+
+<style>
+</style>
